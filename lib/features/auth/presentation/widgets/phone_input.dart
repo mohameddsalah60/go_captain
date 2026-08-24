40d9be690487b3a5d54b2e6c0412ee7aa09c5d4f
mohameddsalah60 <mohamedsalahdev2004@gmail.com>
2026-08-24 21:58:44 +0300

    feat: Update phone input widget layout and adjust text alignment for better UX

--- lib/features/auth/presentation/widgets/phone_input.dart
+++ lib/features/auth/presentation/widgets/phone_input.dart
@@ -28,26 +28,6 @@ class PhoneInput extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 4.h),
           child: Row(
             children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
-                decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.circular(12.r),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('🇪🇬', style: TextStyle(fontSize: 20.sp)),
-                    SizedBox(width: 8.w),
-                    Text(
-                      '+20',
-                      style: AppTextStyles.w700s14.copyWith(
-                        color: AppColors.black,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
               SizedBox(width: 10.w),
               Expanded(
                 child: TextFormField(
@@ -58,22 +38,41 @@ class PhoneInput extends StatelessWidget {
                     LengthLimitingTextInputFormatter(11),
                   ],
                   onChanged: onSaved,
-                  textAlign: TextAlign.right,
+                  textAlign: TextAlign.start,
                   style: AppTextStyles.w600s16.copyWith(color: AppColors.black),
                   decoration: InputDecoration(
-                    isDense: true,
                     border: InputBorder.none,
                     enabledBorder: InputBorder.none,
                     focusedBorder: InputBorder.none,
                     errorBorder: InputBorder.none,
                     focusedErrorBorder: InputBorder.none,
                     hintText: 'رقم الموبايل',
-                    hintTextDirection: TextDirection.rtl,
+                    hintTextDirection: TextDirection.ltr,
                     hintStyle: AppTextStyles.w500s15.copyWith(
                       color: AppColors.grey,
                     ),
                     contentPadding: EdgeInsets.symmetric(vertical: 14.h),
                   ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      '20+',
+                      style: AppTextStyles.w700s14.copyWith(
+                        color: AppColors.black,
+                      ),
+                    ),
+                    SizedBox(width: 8.w),
+                    Text('🇪🇬', style: TextStyle(fontSize: 20.sp)),
+                  ],
                 ),
               ),
             ],
